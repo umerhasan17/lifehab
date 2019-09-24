0ab33802af111fe764cde57cf5f7a8658f904770
umerhasan17 <umerhasan17@gmail.com>
2019-09-24 18:49:20 +0100

    Create empty temp & humidity panels

---
+++ src/components/HumidityPanel/HumidityPanel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Card/Card.css';
+import Slider from '../Slider/Slider';
+import OnOffSwitch from '../Switch/Switch';
+
+export default class HumidityPanel extends React.Component {
+
+    render() {
+        return(<div>HumidityPanel</div>);
+    }
+}
---
+++ src/components/TemperaturePanel/TemperaturePanel.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Card/Card.css';
+import Slider from '../Slider/Slider';
+import OnOffSwitch from '../Switch/Switch';
+
+export default class TemperaturePanel extends React.Component {
+
+    render() {
+        return(<div>TemperaturePanel</div>);
+    }
+}
