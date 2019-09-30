527d037f2e3a3a58f55b5426721061bcefa3c10d
umerhasan17 <umerhasan17@gmail.com>
2019-09-30 10:33:42 +0100

    Add water panel component

---
+++ src/components/WaterPanel/WaterPanel.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../Card/Card.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import SensorReading from '../SensorReading/SensorReading';
+
+export default class WaterPanel extends React.Component {
+    state = {
+        title: "Water Pressure",
+    }
+
+    render() {
+        return (
+            <div className='card-container'>
+                <div className='card-title'>{this.state.title}</div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <SensorReading label="sump1 (kPa)" value="102"/>
+                        </Col>
+                        <Col>
+                            <SensorReading label="sump2 (kPa)" value="103"/>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        )
+    }
+}
